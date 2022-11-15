b0271e1125c7926577a28d55f6fb588f62db0019
Tim Besard <tim.besard@gmail.com>
2022-11-15 14:10:15 +0100

    Fixes for testsuite.

--- test/threads.jl
+++ test/threads.jl
@@ -40,16 +40,18 @@ end
     kill(thread)
     sleep(0.2)
     @test_broken isempty(c)     # Julia seems to ignore the signal...
+    take!(c)                    # ... so we also need the empty the channel
 end
 
 @testset "cancel" begin
     c = Channel()
     thread = pthread() do
-        sleep(0.1)
+        sleep(0.5)
         put!(c, 42)
     end
     @test isempty(c)
+    sleep(0.2)  # give the thread a chance to finish compiling
     pthreads.cancel(thread)
-    sleep(0.2)
+    sleep(0.3)
     @test isempty(c)
 end
